507ce5940e1906ec41af841dacd19130ff2b9c8a
Victor Mendez <victor.mmc.pl@hotmail.com>
2022-12-20 12:37:39 -0600

    Implemented User and Activity components.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
+import { useState } from 'react';
 import './App.scss'
 
+import data from "../data.json";
+
+import User from "./components/User/User";
+import Activity from "./components/Activity/Activity";
+
 function App() {
+  const [activities, setActivities] = useState([])
+  const [timeset, setTimeset] = useState('weekly')
+  
+  fetch("../data.json")
+  .then(res => res.json())
+  .then( data => setActivities(data) )
+
   return (
     <div className="App">
+      <User />
+      
+      <section id='activities'>
+        { activities.map((activity, key) => <Activity key={key} title={activity.title} timeframes={activity.timeframes} timeset={timeset} />) }
+      </section>
     </div>
   )
 }
