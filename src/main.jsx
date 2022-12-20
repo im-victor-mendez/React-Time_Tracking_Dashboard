93256fa36d4e02ec19f4da5727ab77caf22965a4
Victor Mendez <victor.mmc.pl@hotmail.com>
2022-12-20 13:33:21 -0600

    Styled for mobile sizes.

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import './const/colors.scss'
 import './const/fonts.scss'
 import './main.scss'
+import './styles/Mobile.scss'
 import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
