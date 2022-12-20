6dbf7c3bd6e8829b1e627fd4a7cbe1e35513250d
Victor Mendez <victor.mmc.pl@hotmail.com>
2022-12-19 20:33:55 -0600

    Implemented main and constants SASS files.

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import './const/colors.scss'
+import './const/fonts.scss'
+import './main.scss'
 import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
